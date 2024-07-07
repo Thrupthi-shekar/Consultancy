d9a470efeaabf4f918d48a6cb9e2e13e7ebca0c9
Thrupthi shekar <thrupthishekar32@gmail.com>
2024-07-07 14:20:32 +0530

    update

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import { BrowserRouter, Routes,Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-    <BrowserRouter>
+    <BrowserRouter basename="/Consultancy">
       <Routes>
         <Route index element={<Home/>}/>
         <Route path="/home" element={<HomePage/>}/>
